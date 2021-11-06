2bf838893a05b60d3b20a7e73d640aba73d76549
Jordan Yelloz <jordan@yelloz.me>
2021-11-05 18:43:49 -0700

    server: Added server-side chat representation.
    It supports formatting a chat message sent by a specific nickname into
    the expected Hotline representation by starting with a carriage return
    and the nickname followed by a colon.

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,1 +1,24 @@
 pub mod files;
+
+#[derive(Debug)]
+pub struct Chat(pub User, pub Vec<u8>);
+
+impl Into<ChatMessage> for Chat {
+    fn into(self) -> ChatMessage {
+        let Self(user, text) = self;
+        let User(username) = user;
+        let message = [
+            &b"\r "[..],
+            &username[..],
+            &b": "[..],
+            &text[..],
+        ].concat();
+        ChatMessage {
+            chat_id: None,
+            message,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct User(pub Vec<u8>);
