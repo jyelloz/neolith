03d47681d5fedf80477067f46685d64b4fedd29b
Jordan Yelloz <jordan@yelloz.me>
2021-11-11 15:39:14 -0800

    users: Converted to key-value store.
    It's now a HashSet instead of a Vec.

--- src/server/users.rs
+++ src/server/users.rs
@@ -3,39 +3,70 @@ use crate::protocol::{
     UserNameWithInfo,
 };
 
+use derive_more::{From, Into};
+
+use std::collections::HashSet;
+
 #[derive(Debug, Clone)]
-pub struct Users(Vec<UserNameWithInfo>, usize);
+pub struct Users(HashSet<User>, i16);
+
+#[derive(Debug, Clone, From, Into, Eq)]
+struct User(UserNameWithInfo);
+
+impl std::hash::Hash for User {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        i16::from(self.0.user_id).hash(state);
+    }
+}
+
+impl std::cmp::PartialEq for User {
+    fn eq(&self, other: &Self) -> bool {
+        self.0.user_id == other.0.user_id
+    }
+}
 
 impl Users {
     pub fn new() -> Self {
-        Self(vec![], 1)
+        Self(HashSet::new(), 1)
     }
     pub fn add(&mut self, user: &mut UserNameWithInfo) -> UserId {
         let Self(users, top_id) = self;
-        let user_id = (*top_id as i16).into();
+        let user_id = (*top_id).into();
         user.user_id = user_id;
         *top_id += 1;
-        users.push(user.clone());
+        users.insert(user.clone().into());
         user_id
     }
     pub fn update(&mut self, user: &UserNameWithInfo) -> Option<UserId> {
         let Self(users, ..) = self;
-        if let Some(u) = users.iter_mut().find(|u| u.user_id == user.user_id) {
-            *u = user.clone();
-            Some(user.user_id)
+        let user = User::from(user.clone());
+        if let Some(u) = &mut users.get(&user){
+            *u = &user;
+            Some(user.0.user_id)
         } else {
             None
         }
     }
     pub fn remove(&mut self, user: &UserNameWithInfo) {
         let Self(users, ..) = self;
-        users.retain(|u| u.user_id != user.user_id);
-    }
-    pub fn borrow(&self) -> &[UserNameWithInfo] {
-        &self.0
+        let user = User::from(user.clone());
+        users.remove(&user);
     }
     pub fn find(&self, id: UserId) -> Option<&UserNameWithInfo> {
+        let Self(users, ..) = self;
+        let fake_user = UserNameWithInfo {
+            user_id: id,
+            user_flags: 0.into(),
+            icon_id: 0.into(),
+            username: b"".to_vec().into(),
+        };
+        users.get(&fake_user.into())
+            .map(|u| &u.0)
+    }
+    pub fn to_vec(&self) -> Vec<UserNameWithInfo> {
         self.0.iter()
-            .find(|u| u.user_id == id)
+            .cloned()
+            .map(User::into)
+            .collect()
     }
 }
