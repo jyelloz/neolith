cd26c74efa99dff29bbc64935c77e049c74c202e
Jordan Yelloz <jordan@yelloz.me>
2021-11-24 11:12:32 -0800

    Created connection module.
    This contains the connection wrapper that knows how to read/write frames. It
    represents a connection that has already gone through the handshake process so
    it should be usable from a client or server implementation.
    
    It should eventually replace the transaction stream module.

---
+++ src/connection.rs
@@ -0,0 +1,52 @@
+use tokio::io::{
+    AsyncRead,
+    AsyncWrite,
+    AsyncReadExt as _,
+    AsyncWriteExt as _,
+};
+
+use super::protocol::{
+    TransactionHeader,
+    TransactionBody,
+    TransactionFrame,
+    ProtocolError,
+    HotlineProtocol as _,
+};
+
+type Result<T> = ::core::result::Result<T, ProtocolError>;
+
+pub struct Connection<S> {
+    socket: S,
+}
+
+impl <S: AsyncRead + AsyncWrite + Unpin> Connection<S> {
+    pub async fn read_frame(&mut self) -> Result<TransactionFrame> {
+        let header = self.header().await?;
+        let size = header.body_len();
+        let body = self.body(size).await?;
+        Ok(TransactionFrame { header, body })
+    }
+    async fn header(&mut self) -> Result<TransactionHeader> {
+        let Self { socket } = self;
+        let mut buf = [0u8; 20];
+        socket.read_exact(&mut buf).await?;
+        match TransactionHeader::from_bytes(&buf) {
+            Ok((_, header)) => Ok(header),
+            Err(_) => Err(ProtocolError::ParseHeader),
+        }
+    }
+    async fn body(&mut self, size: usize) -> Result<TransactionBody> {
+        let Self { socket } = self;
+        let mut buf = &mut vec![0u8; size][..size];
+        socket.read_exact(&mut buf).await?;
+        match TransactionBody::from_bytes(&buf) {
+            Ok((_, body)) => Ok(body),
+            Err(_) => Err(ProtocolError::ParseBody),
+        }
+    }
+    pub async fn write_frame(&mut self, frame: TransactionFrame) -> Result<()> {
+        self.socket.write_all(&frame.into_bytes()).await?;
+        Ok(())
+    }
+
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 pub mod protocol;
+pub mod connection;
 pub mod server;
 pub mod apple;
