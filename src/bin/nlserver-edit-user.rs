3ce025cb14313572b1f6be8e6b86db294b58bcae
Jordan Yelloz <jordan@yelloz.me>
2022-12-06 22:08:11 -0700

    bin: Added user edit tool.

---
+++ src/bin/nlserver-edit-user.rs
@@ -0,0 +1,33 @@
+use std::fs;
+
+use anyhow::{Result, anyhow};
+use dialoguer::Confirm;
+
+use neolith::server::user_editor::InteractiveUserEditor;
+
+fn main() -> Result<()> {
+    let args = std::env::args();
+
+    let filename = if let Some(filename) = args.skip(1).next() {
+        Ok(filename)
+    } else {
+        Err(anyhow!("provide input filename"))
+    }?;
+
+    let input = fs::read(&filename)?;
+    let output = InteractiveUserEditor::deserialize(&input)?
+        .interact()?
+        .serialize()?;
+
+    let write = Confirm::new()
+        .with_prompt("Are you sure you want to modify this user?")
+        .interact()?;
+    if write {
+        fs::write(&filename, output)?;
+        eprintln!("updated");
+    } else {
+        eprintln!("cancelled");
+    }
+
+    Ok(())
+}
