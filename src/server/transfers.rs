34a081bdaf7771095a8399d407b2865a55616206
Jordan Yelloz <jordan@yelloz.me>
2025-01-05 17:52:26 -0700

    transfers: Moved more code to OsFiles

--- src/server/transfers.rs
+++ src/server/transfers.rs
@@ -3,6 +3,7 @@ use derive_more::{From, Into};
 use std::{
     collections::HashMap,
     io::{self, SeekFrom},
+    num::TryFromIntError,
     path::{Path, PathBuf},
 };
 use thiserror::Error;
@@ -15,7 +16,7 @@ use tracing::{debug, error, warn};
 
 use crate::apple;
 use crate::protocol::{self as proto, HotlineProtocol, ReferenceNumber};
-use crate::server::bus::Bus;
+use crate::server::{bus::Bus, files::OsFiles};
 
 #[derive(Debug, Error)]
 pub enum TransferError {
@@ -95,147 +96,12 @@ impl Requests {
 }
 
 #[derive(Debug, Clone)]
-pub struct Files(pub PathBuf);
+struct Files(PathBuf);
 
 impl Files {
-    pub fn with_root(root: &Path) -> Self {
-        Self(root.to_owned())
-    }
     fn child(&self, path: &Path) -> PathBuf {
         let Self(root) = self;
         root.clone().join(path)
-    }
-    async fn read(&self, path: &Path, offset: u64) -> io::Result<fs::File> {
-        let path = self.child(path);
-        let mut file = fs::File::open(path).await?;
-        file.seek(SeekFrom::Start(offset)).await?;
-        Ok(file)
-    }
-    fn get_appledouble(path: &Path) -> PathBuf {
-        let basename = path
-            .file_name()
-            .and_then(|name| name.to_str())
-            .map(|name| format!("._{}", name))
-            .expect("no filename");
-        path.to_path_buf().with_file_name(basename)
-    }
-    fn get_filename(path: &Path) -> Vec<u8> {
-        path.file_name()
-            .expect("no file name")
-            .to_str()
-            .expect("filename must be a str")
-            .to_string()
-            .into_bytes()
-    }
-    async fn get_appledouble_file(&self, path: &Path) -> io::Result<fs::File> {
-        let ad_path = Self::get_appledouble(path);
-        self.read(&ad_path, 0).await
-    }
-    async fn get_appledouble_info(&self, path: &Path) -> io::Result<(u64, proto::InfoFork)> {
-        let mut file = self.get_appledouble_file(path).await?;
-        let mut buf = [0u8; apple::AppleSingleHeaderStub::calculate_size()];
-        file.read_exact(&mut buf).await?;
-        let header = apple::AppleSingleHeaderStub::try_from(&buf[..])?;
-        let mut entries = vec![];
-        for _ in 0..header.n_descriptors {
-            let mut buf = [0u8; apple::EntryDescriptor::calculate_size()];
-            file.read_exact(&mut buf).await?;
-            let entry = apple::EntryDescriptor::try_from(&buf[..])?;
-            entries.push(entry);
-        }
-
-        let rsrc_len = entries
-            .iter()
-            .filter_map(|e| e.rsrc())
-            .map(|e| e.length as u64)
-            .sum::<u64>();
-
-        // TODO: use the info entry and seek to position
-        let mut buf = [0u8; apple::FinderInfo::calculate_size()];
-        file.read_exact(&mut buf).await?;
-
-        let info = apple::FinderInfo::try_from(&buf[..])?;
-        let file_name = Self::get_filename(path);
-
-        let fork = proto::InfoFork {
-            platform: proto::PlatformType::AppleMac,
-            type_code: proto::FileType(info.file_type.0 .0),
-            creator_code: proto::Creator(info.creator.0 .0),
-            flags: Default::default(),
-            platform_flags: Default::default(),
-            created_at: Default::default(),
-            modified_at: Default::default(),
-            name_script: Default::default(),
-            name_len: file_name.len() as i16,
-            file_name,
-            comment_len: 0,
-            comment: vec![],
-        };
-        Ok((rsrc_len, fork))
-    }
-    pub async fn info(&self, path: &Path) -> io::Result<(u64, proto::InfoFork)> {
-        debug!("info on {path:?}, root={:?}", self.0);
-        let fs_path = self.child(path);
-        let stat = fs::metadata(&fs_path).await?;
-        debug!("found stat {stat:?}");
-        let file_name = fs_path
-            .file_name()
-            .expect("no file name")
-            .to_str()
-            .expect("filename must be a str")
-            .to_string()
-            .into_bytes();
-        debug!("found filename {file_name:?}");
-        let len = stat.len();
-        let (len, info) = match self.get_appledouble_info(path).await {
-            Ok((rsrc_len, info)) => {
-                debug!("for appledouble'd file {path:?} have data length {len}, rsrc length {rsrc_len}");
-                (len + rsrc_len, info)
-            }
-            Err(e) => {
-                debug!("no appledouble info: {e:?}");
-                let info = proto::InfoFork {
-                    platform: proto::PlatformType::AppleMac,
-                    type_code: proto::FileType::try_from(&b"BINA"[..]).unwrap(),
-                    creator_code: proto::Creator::try_from(&b"dosa"[..]).unwrap(),
-                    flags: Default::default(),
-                    platform_flags: Default::default(),
-                    created_at: Default::default(),
-                    modified_at: Default::default(),
-                    name_script: Default::default(),
-                    name_len: file_name.len() as i16,
-                    file_name,
-                    comment_len: 0,
-                    comment: vec![],
-                };
-                (len, info)
-            }
-        };
-        debug!("info: {info:?}");
-        Ok((len, info))
-    }
-    pub async fn rsrc(&self, path: &Path) -> io::Result<Option<(u64, fs::File)>> {
-        let Ok(mut file) = self.get_appledouble_file(path).await else {
-            return Ok(None);
-        };
-        debug!("have appledouble file");
-        let mut buf = [0u8; apple::AppleSingleHeaderStub::calculate_size()];
-        file.read_exact(&mut buf).await?;
-        let header = apple::AppleSingleHeaderStub::try_from(&buf[..])?;
-        debug!("have appledouble stub {header:?}");
-        let mut entries = vec![];
-        for _ in 0..header.n_descriptors {
-            let mut buf = [0u8; apple::EntryDescriptor::calculate_size()];
-            file.read_exact(&mut buf).await?;
-            let entry = apple::EntryDescriptor::try_from(&buf[..])?;
-            entries.push(entry);
-        }
-        let Some(rsrc_entry) = entries.iter().filter_map(|e| e.rsrc()).next() else {
-            return Ok(None);
-        };
-        debug!("have rsrc entry {rsrc_entry:?}");
-        file.seek(SeekFrom::Start(rsrc_entry.offset as u64)).await?;
-        Ok(Some((rsrc_entry.length as u64, file)))
     }
     async fn write(
         &self,
@@ -261,6 +127,7 @@ impl Files {
 
 pub struct TransferConnection<S> {
     files: Files,
+    os_files: OsFiles,
     transfers: TransfersService,
     requests: watch::Receiver<Requests>,
     socket: S,
@@ -295,10 +162,12 @@ impl<S: AsyncRead + AsyncWrite + Unpin + Send> TransferConnection<S> {
         transfers: TransfersService,
         requests: watch::Receiver<Requests>,
     ) -> Self {
-        let files = Files(root);
+        let files = Files(root.clone());
+        let os_files = OsFiles::with_root(root.clone()).unwrap();
         Self {
             socket,
             files,
+            os_files,
             transfers,
             requests,
         }
@@ -353,24 +222,13 @@ impl<S: AsyncRead + AsyncWrite + Unpin + Send> TransferConnection<S> {
     }
     async fn handle_file_download(self, id: ReferenceNumber) -> TransferResult<()> {
         let path = self.get_file_download(id)?;
-        debug!("path {path:?}");
+        let appledouble_path = Self::get_appledouble(&path);
         let Self {
-            mut socket, files, ..
+            mut socket,
+            os_files,
+            ..
         } = self;
-        let (len, info) = files.info(&path).await?;
-        debug!("info {info:?}");
-        let file: Box<dyn AsyncRead + Send + Unpin> = Box::new(files.read(&path, 0).await?);
-        let data = (len, file).into();
-        let rsrc = files.rsrc(&path).await?;
-        let mut file = if let Some((len, rsrc)) = rsrc {
-            debug!("data+rsrc");
-            let rsrc: Box<dyn AsyncRead + Send + Unpin> = Box::new(rsrc);
-            let rsrc = (len, rsrc).into();
-            proto::FlattenedFileObject::with_forks(info, data, rsrc)
-        } else {
-            debug!("data only");
-            proto::FlattenedFileObject::with_data(info, data)
-        };
+        let mut file = os_files.read(&path).await?;
         let (info_header, info) = file.info();
         let header = file.header();
         let header = header.to_bytes().unwrap();
@@ -512,14 +370,14 @@ enum Command {
 
 #[derive(Debug, Clone)]
 pub struct TransfersService {
-    bus: Bus,
+    _bus: Bus,
     tx: mpsc::Sender<Command>,
 }
 
 impl TransfersService {
     pub fn new(bus: Bus) -> (Self, TransfersUpdateProcessor) {
         let (tx, rx) = mpsc::channel(10);
-        let service = Self { bus, tx };
+        let service = Self { _bus: bus, tx };
         let process = TransfersUpdateProcessor::new(rx);
         (service, process)
     }
@@ -611,13 +469,16 @@ impl TransfersUpdateProcessor {
         offset: u64,
         requests: &mut Requests,
     ) -> TransferResult<proto::DownloadFileReply> {
-        let files = Files::with_root(root);
-        let (len, info) = files.info(path).await?;
-        let transfer_size = (info.size() as u64 + len - offset) as i32;
+        let files = OsFiles::with_root(root)?;
+        let file = files.read(path).await?;
+        let file_size = file.fork_len(proto::ForkType::Data).unwrap_or(0)
+            + file.fork_len(proto::ForkType::Resource).unwrap_or(0);
+        let (_, info) = file.info();
+        let transfer_size = info.size() as u64 + file_size as u64 - offset;
         let reference = requests.add_download(root.to_path_buf(), path.to_path_buf());
         let reply = proto::DownloadFileReply {
-            transfer_size: transfer_size.into(),
-            file_size: (len as i32).into(),
+            transfer_size: transfer_size.try_into()?,
+            file_size: file_size.try_into()?,
             reference,
             waiting_count: None,
         };
