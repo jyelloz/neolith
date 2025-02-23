794008b4c0149b2917f07c44e5df41bbc4de8380
Jordan Yelloz <jordan@yelloz.me>
2025-02-23 16:48:23 -0700

    sansio: Standardized a single trait for all parsers

--- examples/sansio.rs
+++ examples/sansio.rs
@@ -1,20 +1,30 @@
+use bytes::Buf as _;
 use deku::prelude::*;
 use neolith::protocol as proto;
-use std::io::{Cursor, prelude::*};
-use bytes::Buf;
+use std::io::{self, prelude::*, Cursor};
+
+type ParseResponse<O> = (usize, Option<O>);
+trait Parser {
+    type Output;
+    fn parse(&mut self, buf: &[u8]) -> ParseResponse<Self::Output>;
+}
 
 #[derive(Default)]
-pub struct HeaderReader(Cursor<[u8; Self::SIZE]>);
-impl HeaderReader {
+pub struct HeaderParser(Cursor<[u8; Self::SIZE]>);
+impl HeaderParser {
     const SIZE: usize = 20;
-    pub fn parse(&mut self, buf: &[u8]) -> (usize, Option<proto::TransactionHeader>) {
+}
+impl Parser for HeaderParser {
+    type Output = proto::TransactionHeader;
+    fn parse(&mut self, buf: &[u8]) -> ParseResponse<Self::Output> {
         let Self(cursor) = self;
         let need = cursor.remaining();
         let len = need.min(buf.len());
         let buf = &buf[..len];
         cursor.write_all(buf).expect("failed to copy to cursor");
         if cursor.remaining() == 0 {
-            let (_, header) = proto::TransactionHeader::from_bytes((cursor.get_ref(), 0)).expect("failed to read header");
+            let (_, header) = proto::TransactionHeader::from_bytes((cursor.get_ref(), 0))
+                .expect("failed to read header");
             (len, Some(header))
         } else {
             (len, None)
@@ -23,10 +33,13 @@ impl HeaderReader {
 }
 
 #[derive(Default)]
-pub struct ParameterCountReader(Cursor<[u8; Self::SIZE]>);
-impl ParameterCountReader {
+pub struct ParameterCountParser(Cursor<[u8; Self::SIZE]>);
+impl ParameterCountParser {
     const SIZE: usize = size_of::<u16>();
-    pub fn parse(&mut self, buf: &[u8]) -> (usize, Option<usize>) {
+}
+impl Parser for ParameterCountParser {
+    type Output = usize;
+    fn parse(&mut self, buf: &[u8]) -> ParseResponse<Self::Output> {
         let Self(cursor) = self;
         let need = cursor.remaining();
         let len = need.min(buf.len());
@@ -42,17 +55,21 @@ impl ParameterCountReader {
 }
 
 #[derive(Default)]
-pub struct FieldIdReader(Cursor<[u8; Self::SIZE]>);
-impl FieldIdReader {
+pub struct FieldIdParser(Cursor<[u8; Self::SIZE]>);
+impl FieldIdParser {
     const SIZE: usize = size_of::<i16>();
-    pub fn parse(&mut self, buf: &[u8]) -> (usize, Option<proto::FieldId>) {
+}
+impl Parser for FieldIdParser {
+    type Output = proto::FieldId;
+    fn parse(&mut self, buf: &[u8]) -> ParseResponse<Self::Output> {
         let Self(cursor) = self;
         let need = cursor.remaining();
         let len = need.min(buf.len());
         let buf = &buf[..len];
         cursor.write_all(buf).expect("failed to copy to cursor");
         if cursor.remaining() == 0 {
-            let (_, id) = proto::FieldId::from_bytes((cursor.get_ref(), 0)).expect("failed to read field id");
+            let (_, id) =
+                proto::FieldId::from_bytes((cursor.get_ref(), 0)).expect("failed to read field id");
             (len, Some(id))
         } else {
             (len, None)
@@ -61,17 +78,20 @@ impl FieldIdReader {
 }
 
 #[derive(Default)]
-pub struct FieldSizeReader(Cursor<[u8; Self::SIZE]>);
-impl FieldSizeReader {
+pub struct FieldSizeParser(Cursor<[u8; Self::SIZE]>);
+impl FieldSizeParser {
     const SIZE: usize = size_of::<u16>();
-    pub fn parse(&mut self, buf: &[u8]) -> (usize, Option<usize>) {
+}
+impl Parser for FieldSizeParser {
+    type Output = usize;
+    fn parse(&mut self, buf: &[u8]) -> ParseResponse<Self::Output> {
         let Self(cursor) = self;
         let need = cursor.remaining();
         let len = need.min(buf.len());
         let buf = &buf[..len];
         cursor.write_all(buf).expect("failed to copy to cursor");
         if cursor.remaining() == 0 {
-            let val = u16::from_be_bytes(*cursor.get_ref());
+            let val = i16::from_be_bytes(*cursor.get_ref());
             (len, Some(val as usize))
         } else {
             (len, None)
@@ -79,14 +99,15 @@ impl FieldSizeReader {
     }
 }
 
-pub struct FieldDataReader(Cursor<Box<[u8]>>);
-impl FieldDataReader {
+pub struct FieldDataParser(Cursor<Box<[u8]>>);
+impl FieldDataParser {
     pub fn new(data_size: usize) -> Self {
-        Self (
-             Cursor::new(vec![0u8; data_size].into_boxed_slice()),
-        )
+        Self(Cursor::new(vec![0u8; data_size].into_boxed_slice()))
     }
-    pub fn parse(&mut self, buf: &[u8]) -> (usize, Option<Vec<u8>>) {
+}
+impl Parser for FieldDataParser {
+    type Output = Vec<u8>;
+    fn parse(&mut self, buf: &[u8]) -> ParseResponse<Self::Output> {
         let Self(pending_data) = self;
         let read_len = buf.len().min(pending_data.remaining());
         let buf = &buf[..read_len];
@@ -101,20 +122,31 @@ impl FieldDataReader {
 }
 
 enum TransactionParseState {
-    Header(HeaderReader),
-    ParameterCount(ParameterCountReader),
-    ParameterFieldId(FieldIdReader),
-    ParameterFieldSize(FieldSizeReader),
-    ParameterFieldData(FieldDataReader),
+    Header(HeaderParser),
+    ParameterCount(ParameterCountParser),
+    ParameterFieldId(FieldIdParser),
+    ParameterFieldSize(FieldSizeParser),
+    ParameterFieldData(FieldDataParser),
 }
 
 impl Default for TransactionParseState {
     fn default() -> Self {
-        Self::Header(HeaderReader::default())
+        Self::Header(HeaderParser::default())
     }
 }
 
-struct TransactionParseResponse(usize, Option<proto::TransactionFrame>);
+impl std::fmt::Debug for TransactionParseState {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let text = match self {
+            Self::Header(_) => "Header",
+            Self::ParameterCount(_) => "ParameterCount",
+            Self::ParameterFieldId(_) => "ParameterFieldId",
+            Self::ParameterFieldSize(_) => "ParameterFieldSize",
+            Self::ParameterFieldData(_) => "ParameterFieldData",
+        };
+        f.write_str(text)
+    }
+}
 
 struct TransactionParser {
     state: TransactionParseState,
@@ -123,7 +155,6 @@ struct TransactionParser {
     current_param: proto::Parameter,
     params: Vec<proto::Parameter>,
 }
-
 impl Default for TransactionParser {
     fn default() -> Self {
         Self {
@@ -133,181 +164,106 @@ impl Default for TransactionParser {
             params: vec![],
             current_param: proto::Parameter {
                 field_id: 0.into(),
-                field_size: 0i16.into(),
+                field_size: 0u16.into(),
                 field_data: vec![],
             },
         }
     }
 }
-
-impl TransactionParser {
-    pub fn parse(&mut self, data: &[u8]) -> TransactionParseResponse {
+impl Parser for TransactionParser {
+    type Output = proto::TransactionFrame;
+    fn parse(&mut self, buf: &[u8]) -> ParseResponse<Self::Output> {
         let len = match &mut self.state {
             TransactionParseState::Header(rdr) => {
-                let (len, header) = rdr.parse(&data);
+                let (len, header) = rdr.parse(&buf);
                 let Some(header) = header else {
-                    eprintln!("not enough data for header");
-                    return TransactionParseResponse(0, None)
+                    return (len, None);
                 };
-                if i32::from(header.data_size) <= len as i32 {
-                    self.state = TransactionParseState::Header(HeaderReader::default());
-                    let transaction = proto::TransactionFrame::empty(header);
-                    return TransactionParseResponse(len, Some(transaction))
-                }
-                self.state = TransactionParseState::ParameterCount(ParameterCountReader::default());
+                self.state = TransactionParseState::ParameterCount(ParameterCountParser::default());
+                self.hdr.replace(header);
                 len
             }
             TransactionParseState::ParameterCount(rdr) => {
-                let (len, count) = rdr.parse(data);
+                let (len, count) = rdr.parse(buf);
                 let Some(count) = count else {
-                    return TransactionParseResponse(len, None)
+                    return (len, None);
                 };
                 if count == 0 {
-                    self.state = TransactionParseState::Header(HeaderReader::default());
+                    self.state = TransactionParseState::Header(HeaderParser::default());
                     let header = self.hdr.take().unwrap();
                     let transaction = proto::TransactionFrame::empty(header);
-                    return TransactionParseResponse(len, Some(transaction))
+                    return (len, Some(transaction));
                 }
                 self.param_count = count;
-                self.state = TransactionParseState::ParameterFieldId(FieldIdReader::default());
+                self.state = TransactionParseState::ParameterFieldId(FieldIdParser::default());
                 len
-            },
+            }
             TransactionParseState::ParameterFieldId(rdr) => {
-                let (len, id) = rdr.parse(data);
+                let (len, id) = rdr.parse(buf);
                 let Some(id) = id else {
-                    eprintln!("not enough data for field id");
-                    return TransactionParseResponse(len, None)
+                    return (len, None);
                 };
                 self.current_param.field_id = id;
-                self.state = TransactionParseState::ParameterFieldSize(FieldSizeReader::default());
+                self.state = TransactionParseState::ParameterFieldSize(FieldSizeParser::default());
                 len
-            },
+            }
             TransactionParseState::ParameterFieldSize(rdr) => {
-                let (len, size) = rdr.parse(data);
+                let (len, size) = rdr.parse(buf);
                 let Some(size) = size else {
-                    return TransactionParseResponse(len, None)
+                    return (len, None);
                 };
-                self.current_param.field_size = size as i16;
-                self.state = TransactionParseState::ParameterFieldData(FieldDataReader::new(size));
+                self.current_param.field_size = size as u16;
+                self.state = TransactionParseState::ParameterFieldData(FieldDataParser::new(size));
                 len
-            },
+            }
             TransactionParseState::ParameterFieldData(rdr) => {
-                let (len, field_data) = rdr.parse(data);
+                let (len, field_data) = rdr.parse(buf);
                 let Some(field_data) = field_data else {
-                    eprintln!("not enough data for field data");
-                    return TransactionParseResponse(len, None)
+                    return (len, None);
                 };
                 self.current_param.field_data = field_data;
                 self.params.push(self.current_param.clone());
-                self.param_count -= 1;
+                self.param_count = self.param_count.saturating_sub(1);
                 if self.param_count == 0 {
                     let header = self.hdr.take().unwrap();
                     let transaction = proto::TransactionFrame::new(header, self.params.clone());
-                    eprintln!("transaction {transaction:?}");
                     self.params = vec![];
-                    self.state = TransactionParseState::Header(HeaderReader::default());
-                    return TransactionParseResponse(len, Some(transaction))
+                    self.state = TransactionParseState::Header(HeaderParser::default());
+                    return (len, Some(transaction));
                 }
-                self.state = TransactionParseState::ParameterFieldId(FieldIdReader::default());
+                self.state = TransactionParseState::ParameterFieldId(FieldIdParser::default());
                 len
-            },
+            }
         };
-        TransactionParseResponse(len, None)
+        (len, None)
     }
 }
 
 fn main() -> anyhow::Result<()> {
-    let data = include_bytes!("../packets/edit-user-error.bin");
-    let data = [&data[..], &data[..], &[0u8; 12], &123u32.to_be_bytes(), &456u32.to_be_bytes(), &[0u8; 2]].concat();
-    let mut data = &data[..];
-    let mut state = TransactionParseState::Header(HeaderReader::default());
-    let mut hdr = None::<proto::TransactionHeader>;
-    let mut param_count = 0;
-    let mut params = vec![];
-    let mut current_param = proto::Parameter {
-        field_id: 0.into(),
-        field_size: 0i16.into(),
-        field_data: vec![],
-    };
-    while data.len() > 0 {
-        eprintln!("{} bytes to go", data.len());
-        match &mut state {
-            TransactionParseState::Header(rdr) => {
-                let (len, header) = rdr.parse(&data);
-                data = &data[len..];
-                let Some(header) = header else {
-                    eprintln!("not enough data for header");
-                    continue;
-                };
-                hdr.replace(header);
-                if i32::from(header.data_size) <= len as i32 {
-                    eprintln!("no transaction body");
-                    state = TransactionParseState::Header(HeaderReader::default());
-                    let header = hdr.take().unwrap();
-                    let transaction = proto::TransactionFrame::empty(header);
-                    eprintln!("transaction {transaction:?}");
-                    continue;
-                }
-                state = TransactionParseState::ParameterCount(ParameterCountReader::default());
-            }
-            TransactionParseState::ParameterCount(rdr) => {
-                let (len, count) = rdr.parse(data);
-                data = &data[len..];
-                let Some(count) = count else {
-                    eprintln!("not enough data for parameter count");
-                    continue;
-                };
-                if count == 0 {
-                    state = TransactionParseState::Header(HeaderReader::default());
-                    let header = hdr.take().unwrap();
-                    let transaction = proto::TransactionFrame::empty(header);
-                    eprintln!("transaction {transaction:?}");
-                } else {
-                    param_count = count;
-                    state = TransactionParseState::ParameterFieldId(FieldIdReader::default());
-                }
-            },
-            TransactionParseState::ParameterFieldId(rdr) => {
-                let (len, id) = rdr.parse(data);
-                data = &data[len..];
-                let Some(id) = id else {
-                    eprintln!("not enough data for field id");
-                    continue;
-                };
-                current_param.field_id = id;
-                state = TransactionParseState::ParameterFieldSize(FieldSizeReader::default());
-            },
-            TransactionParseState::ParameterFieldSize(rdr) => {
-                let (len, size) = rdr.parse(data);
-                data = &data[len..];
-                let Some(size) = size else {
-                    eprintln!("not enough data for field size");
-                    continue;
-                };
-                current_param.field_size = size as i16;
-                state = TransactionParseState::ParameterFieldData(FieldDataReader::new(size));
-            },
-            TransactionParseState::ParameterFieldData(rdr) => {
-                let (len, field_data) = rdr.parse(data);
-                data = &data[len..];
-                let Some(field_data) = field_data else {
-                    eprintln!("not enough data for field data");
-                    continue;
-                };
-                current_param.field_data = field_data;
-                params.push(current_param.clone());
-                param_count -= 1;
-                if param_count == 0 {
-                    let header = hdr.take().unwrap();
-                    let transaction = proto::TransactionFrame::new(header, params.clone());
-                    eprintln!("transaction {transaction:?}");
-                    params = vec![];
-                    state = TransactionParseState::Header(HeaderReader::default());
-                } else {
-                    state = TransactionParseState::ParameterFieldId(FieldIdReader::default());
-                }
-            },
+    let mut parser = TransactionParser::default();
+    let mut stdin = io::stdin();
+    let mut buf = [0u8; 128];
+    {
+        let handshake_len = 12;
+        let mut handshake = [0u8; 1];
+        for _ in 0..handshake_len {
+            stdin.read_exact(&mut handshake)?;
         }
     }
+    loop {
+        let len = stdin.read(&mut buf)?;
+        if len == 0 {
+            break;
+        }
+        let mut buf = &buf[..len];
+        while buf.len() > 0 {
+            let (len, frame) = parser.parse(buf);
+            buf = &buf[len..];
+            if let Some(frame) = frame {
+                eprintln!("{frame:?}");
+            }
+        }
+    }
+    eprintln!("done");
     Ok(())
 }
