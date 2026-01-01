6f7f96cb34276321b3f211442f6293543b2ee029
Jordan Yelloz <jordan@yelloz.me>
2026-01-01 11:10:33 +0100

    transaction_stream: Added sync version of protocol framer

--- src/server/transaction_stream.rs
+++ src/server/transaction_stream.rs
@@ -18,6 +18,46 @@ impl<R> Frames<R> {
     }
 }
 
+impl<R: Read> Frames<R> {
+    fn header_sync(&mut self) -> Result<Option<proto::TransactionHeader>> {
+        let Self(reader) = self;
+        let mut buf = [0u8; proto::TransactionHeader::SIZE_BYTES.unwrap()];
+        match reader.read_exact(&mut buf) {
+            Ok(_) => {},
+            Err(e) => match e.kind() {
+                io::ErrorKind::UnexpectedEof => return Ok(None),
+                _ => Err(e)?,
+            }
+        }
+        proto::TransactionHeader::from_bytes(&buf).map(Some)
+    }
+    fn body_sync(&mut self, size: usize) -> Result<proto::TransactionBody> {
+        let Self(reader) = self;
+        let buf = &mut vec![0u8; size][..size];
+        reader.read_exact(buf)?;
+        proto::TransactionBody::from_bytes(buf)
+    }
+    fn next_frame_sync(&mut self) -> Result<Option<proto::TransactionFrame>> {
+        let Some(header) = self.header_sync()? else {
+            return Ok(None);
+        };
+        let body = self.body_sync(header.body_len())?;
+        Ok(Some(proto::TransactionFrame { header, body }))
+    }
+}
+
+impl<R: Read> Iterator for Frames<R> {
+    type Item = Result<proto::TransactionFrame>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.next_frame_sync() {
+            Ok(Some(f)) => Some(Ok(f)),
+            Ok(None) => None,
+            Err(e) => Some(Err(e)),
+        }
+    }
+}
+
 impl<R: AsyncRead + Unpin> Frames<R> {
     pub fn frames(mut self) -> impl Stream<Item = Result<proto::TransactionFrame>> {
         stream! {
