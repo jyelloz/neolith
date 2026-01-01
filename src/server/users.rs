6f57be654a4cc936ca473c0bb517df381c53d755
Jordan Yelloz <jordan@yelloz.me>
2026-01-01 21:21:29 +0100

    users: cargo fmt

--- src/server/users.rs
+++ src/server/users.rs
@@ -214,7 +214,9 @@ impl UserAccounts {
     }
     async fn load(path: &Path) -> anyhow::Result<HashMap<String, UserAccount>> {
         let mut users: HashMap<String, UserAccount> = HashMap::default();
-        let mut dir = fs::read_dir(path).await.context(format!("user directory: {path:?}"))?;
+        let mut dir = fs::read_dir(path)
+            .await
+            .context(format!("user directory: {path:?}"))?;
         while let Some(file) = dir.next_entry().await? {
             let path = file.path();
             let Ok(data) = fs::read_to_string(&path).await else {
