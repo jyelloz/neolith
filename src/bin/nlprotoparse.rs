ce47f6bd3099314191321b0562c983e8dd145c61
Jordan Yelloz <jordan@yelloz.me>
2026-01-02 12:07:58 +0100

    Added binary for processing packet captures

---
+++ src/bin/nlprotoparse.rs
@@ -0,0 +1,40 @@
+use std::io::stdin;
+use clap::{Parser, ValueEnum};
+
+use deku::{DekuContainerRead as _, noseek::NoSeek};
+use neolith::{protocol as proto, server::transaction_stream::Frames};
+
+#[derive(ValueEnum, Copy, Clone, Debug, PartialEq, Eq)]
+enum Mode {
+    Server,
+    Client,
+}
+
+#[derive(Parser, Debug)]
+#[command(name="nlprotoparse")]
+struct Command {
+    mode: Mode,
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = Command::parse();
+    let stdin = stdin().lock();
+    let mut stdin = NoSeek::new(stdin);
+    match args.mode {
+        Mode::Server => {
+            let (_, hs) = proto::ServerHandshakeReply::from_reader((&mut stdin, 0))?;
+            eprintln!("server to client mode {hs:?}");
+        }
+        Mode::Client => {
+            let (_, hs) = proto::ClientHandshakeRequest::from_reader((&mut stdin, 0))?;
+            eprintln!("client to server mode {hs:?}");
+        },
+    }
+    let frames = Frames::new(stdin);
+    for f in frames {
+        let f = f?;
+        let tt = f.header.transaction_type();
+        eprintln!("frame {tt:?} {f:?}");
+    }
+    Ok(())
+}
