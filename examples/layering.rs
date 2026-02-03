8c500a122ddba23cd56403b8752be638b50bf4f8
Jordan Yelloz <jordan@yelloz.me>
2026-02-03 12:58:36 +0100

    Added example for layering frames into request/response

---
+++ examples/layering.rs
@@ -0,0 +1,26 @@
+use neolith::server::connection::handle_conn;
+use tokio::net::TcpListener;
+use tracing_subscriber::{layer::SubscriberExt as _, util::SubscriberInitExt as _};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::EnvFilter::from_default_env())
+        .with(tracing_subscriber::fmt::layer())
+        .try_init()?;
+
+    let listener = TcpListener::bind("127.0.0.1:5500").await?;
+    let mut id = 0u32;
+    loop {
+        let conn_id = id;
+        id += 1;
+        let (socket, addr) = listener.accept().await?;
+        tracing::info!("accept from {addr:?}!");
+        let (r, w) = socket.into_split();
+        tokio::spawn(async move {
+            if let Err(e) = handle_conn(conn_id, r, w).await {
+                eprintln!("bad conn from {addr:?}: {e:?}");
+            }
+        });
+    }
+}
