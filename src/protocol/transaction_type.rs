fef6fc5083e9ea3592a7abf50eb06e2e3818081e
Jordan Yelloz <jordan@yelloz.me>
2026-02-08 11:36:19 +0100

    protocol: Changed TransactionType to unsigned

--- src/protocol/transaction_type.rs
+++ src/protocol/transaction_type.rs
@@ -1,7 +1,7 @@
 use num_enum::{IntoPrimitive, TryFromPrimitive};
 
 #[derive(Debug, Eq, PartialEq, Ord, PartialOrd, IntoPrimitive, TryFromPrimitive)]
-#[repr(i16)]
+#[repr(u16)]
 pub enum TransactionType {
     Reply = 0,
 
