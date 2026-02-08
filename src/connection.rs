9ceab88279eeecfc61250c15c0a53bfdae4278d1
Jordan Yelloz <jordan@yelloz.me>
2026-02-08 20:36:40 +0100

    connection: s/socket/stream/g

--- src/connection.rs
+++ src/connection.rs
@@ -8,12 +8,12 @@ use super::protocol::{
 type Result<T> = ::core::result::Result<T, ProtocolError>;
 
 pub struct Connection<S> {
-    socket: S,
+    stream: S,
 }
 
 impl<S> Connection<S> {
-    pub fn new(socket: S) -> Self {
-        Self { socket }
+    pub fn new(stream: S) -> Self {
+        Self { stream }
     }
 }
 
@@ -25,25 +25,25 @@ impl<S: AsyncRead + AsyncWrite + Unpin> Connection<S> {
         Ok(TransactionFrame { header, body })
     }
     async fn header(&mut self) -> Result<TransactionHeader> {
-        let Self { socket } = self;
+        let Self { stream } = self;
         let mut buf = [0u8; TransactionHeader::SIZE_BYTES.unwrap()];
-        socket.read_exact(&mut buf).await?;
+        stream.read_exact(&mut buf).await?;
         match TransactionHeader::try_from(&buf[..]) {
             Ok(header) => Ok(header),
             Err(_) => Err(ProtocolError::ParseHeader),
         }
     }
     async fn body(&mut self, size: usize) -> Result<TransactionBody> {
-        let Self { socket } = self;
+        let Self { stream } = self;
         let mut buf = vec![0u8; size];
-        socket.read_exact(&mut buf[..size]).await?;
+        stream.read_exact(&mut buf[..size]).await?;
         match TransactionBody::try_from(&buf[..]) {
             Ok(body) => Ok(body),
             Err(_) => Err(ProtocolError::ParseBody),
         }
     }
     pub async fn write_frame(&mut self, frame: TransactionFrame) -> Result<()> {
-        self.socket.write_all(&frame.into_bytes()).await?;
+        self.stream.write_all(&frame.into_bytes()).await?;
         Ok(())
     }
 }
